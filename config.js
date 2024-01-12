c6d5c5ff06c586b469e50cac1c34c8c95d43989f
Utkarsh Sharma <47462195+vishal30982@users.noreply.github.com>
2024-01-12 23:10:13 +0530

    Update config.js
    rechecked config

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-    apiKey: "AIzaSyDUzo9kHnpZvUrHmNccCQmAKr0MChsqJt8",
-    authDomain: "testing-53825.firebaseapp.com",
-    projectId: "testing-53825",
-    storageBucket: "testing-53825.appspot.com",
-    messagingSenderId: "573288785028",
-    appId: "1:573288785028:web:b8a57b18beded49a1aced1",
-    measurementId: "G-WVWP7BVF3S"
-  };
+  apiKey: "AIzaSyDUzo9kHnpZvUrHmNccCQmAKr0MChsqJt8",
+  authDomain: "testing-53825.firebaseapp.com",
+  projectId: "testing-53825",
+  storageBucket: "testing-53825.appspot.com",
+  messagingSenderId: "573288785028",
+  appId: "1:573288785028:web:b8a57b18beded49a1aced1",
+  measurementId: "G-WVWP7BVF3S"
+};
